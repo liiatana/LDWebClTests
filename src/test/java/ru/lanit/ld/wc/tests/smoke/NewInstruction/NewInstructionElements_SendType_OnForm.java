f01b0c9c63da0b12ce2883ac692524225a6e5836
liiatana <31036379+liiatana@users.noreply.github.com>
2020-01-01 20:43:55 +0300

    test: NewInstructionElements_SendType- добавлен тест проверки иконки и подписи к кнопке

--- src/test/java/ru/lanit/ld/wc/tests/smoke/NewInstruction/NewInstructionElements_SendType_OnForm.java
+++ src/test/java/ru/lanit/ld/wc/tests/smoke/NewInstruction/NewInstructionElements_SendType_OnForm.java
@@ -1,8 +1,10 @@
 package ru.lanit.ld.wc.tests.smoke.NewInstruction;
 
+import com.codeborne.selenide.Condition;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
@@ -69,6 +71,32 @@ public class NewInstructionElements_SendType_OnForm extends TestBase {
         wa.newInstructionPage= wa.openNew(instructionType);
 
         assertThat(wa.newInstructionPage.getVisibleSendTypes(), equalTo(SendTypes.PARALLEL));
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @DataProvider
+    public Object[][] instructionType() {
+
+        return new Object[][]{
+                new Object[]{app.focusedUser.getUserTypes().getAnyType(),SendTypes.PARALLEL},
+                new Object[]{app.focusedUser.getUserTypes().getAnyTaskType(),SendTypes.CHAIN},
+        };
+
+    }
+
+    @Epic(value = "Раздел Сообщения")
+    @Feature(value = "Новое сообщение")
+    @Story(value = "Тип рассылки")
+    @Test(  dataProvider = "instructionType",
+            groups="NewInstructionTests",
+            description ="Проверка текста и иконки для типа рассылки" )
+    public void checkSendTypeIconAndText(InstructionType instructionType,SendTypes sType)
+    {
+        wa.newInstructionPage= wa.openNew(instructionType);
+
+        wa.newInstructionPage.sendTypes.get(sType.getId()).$x("div").shouldHave(Condition.text(sType.getIcon())); //название иконки
+        wa.newInstructionPage.sendTypes.get(sType.getId()).parent().$x("span").shouldHave(Condition.text(sType.getName())); //название рассылки
     }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
